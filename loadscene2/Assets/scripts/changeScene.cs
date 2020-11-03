797a59b66b67d9093e4e8551f6f5089030b58e8c
TUD-RSmith <71321440+TUD-RSmith@users.noreply.github.com>
2020-11-03 15:49:33 +0000

    Initial project upload
    Initial project upload - including WebGL files

---
+++ loadscene2/Assets/scripts/changeScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour {
+
+    public string Url = "https://github.com/TUD-RSmith/test-project/tree/branchB";
+
+    public void GoToSene1()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void GoToSene2()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+
+    public void loadUrl()
+    {
+        Application.OpenURL(Url);
+    }
+
+}
